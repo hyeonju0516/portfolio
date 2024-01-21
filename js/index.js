c1eaae068a3a327088f4f74856e7469ae4f05171
DESKTOP-GD3SP7B\User <hana_smile@naver.com>
2024-01-22 01:29:40 +0900

    0121 프로젝트 일부 수정

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 function openModal(modalId) {
     let modal = document.querySelector('.' + modalId);
 
+    console.log(modalId);
+
     document.querySelector(".modal_cover").style.display = "block";
     modal.style.display = "block";
 
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
 }
 
 function closeModal() {
-    document.querySelector(".modal").style.display = "none";
-    document.querySelector(".modal_cover").style.display = "none";
+    document.querySelectorAll(".modal").forEach((modal) => {
+        modal.style.display = "none";
+    });
 
+    document.querySelector(".modal_cover").style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = "visible";
 }
